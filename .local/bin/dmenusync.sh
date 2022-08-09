4af7226c7e98600c55f5541eeb1544cfd81166cc
Holden Thomas <holden@holdenthomas.xyz>
2022-08-09 15:44:58 -0400

    update dmenusync script

--- .local/bin/dmenusync.sh
+++ .local/bin/dmenusync.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-input="$(printf "Website\\nNAS\\nPhone\\nExpansion" | dmenu -i -p "Select option: ")" || exit 1
+input="$(printf "Website\\nNAS\\nTablet\\nExpansion" | dmenu -i -p "Select option: ")" || exit 1
 
 case $input in
 	"Website")
@@ -13,16 +13,15 @@ case $input in
 		esac
 		;;
 	"NAS") $TERMINAL -e zsh -c 'echo "Syncing NasBox in 2 seconds"; sleep 2; rsync -rtgoD -vP --delete /run/media/holden/HDD8T/Sync/ /run/media/holden/Truenas/Storage/Sync/; read';;
-	"Phone")
-		$TERMINAL -e zsh -c 'echo "not ready yet"; read'; exit 1
-		syncPhone="$(printf "Music\\nVideos\\nPhotos\\n" | dmenu -i -p "Choose Phone sync option: ")" || exit 1
-		case $syncPhone in
+	"Tablet")
+		syncTablet="$(printf "Music\\nVideos\\nPhotos\\n" | dmenu -i -p "Choose Tablet sync option: ")" || exit 1
+		case $syncTablet in
 			"Music") $TERMINAL -e zsh -c 'echo "Syncing music in 2 seconds"; sleep 2; \
-				rsync -vrPO --no-perms --inplace --size-only --delete --exclude=.thumbnails --exclude=iTunes --exclude="YouTube Music"/"10 Hours" --exclude="YouTube Music"/Videos /run/media/holden/HDD8T/Sync/Music/ ~/phone/android/Music/; read';;
+				rsync -vrPO --no-perms --inplace --size-only --delete --exclude=.thumbnails --exclude=iTunes --exclude="YouTube Music"/"10 Hours" --exclude="YouTube Music"/Videos /run/media/holden/HDD8T/Sync/Music/ /run/media/holden/android/Music/; read';;
 			"Videos") $TERMINAL -e zsh -c 'echo "Syncing music in 2 seconds"; sleep 2; \
-				rsync -vrPO --no-perms --inplace --size-only --delete /run/media/holden/HDD8T/Sync/Videos/AMV/ ~/phone/android/Videos/AMV/; read';;
+				rsync -vrPO --no-perms --inplace --size-only --delete /run/media/holden/HDD8T/Sync/Videos/AMV/ /run/media/holden/android/Videos/AMV/; read';;
 			"Photos") $TERMINAL -e zsh -c 'echo "Syncing music in 2 seconds"; sleep 2; \
-				rsync -vrPO --no-perms --inplace --size-only --delete --exclude="iPod Photo Cache" /run/media/holden/HDD8T/Sync/Pictures/"Itunes Pictures"/ ~/phone/android/Pictures/"Itunes Pictures"/; read';;
+				rsync -vrPO --no-perms --inplace --size-only --delete --exclude="iPod Photo Cache" /run/media/holden/HDD8T/Sync/Pictures/Itunes\ Pictures/ /run/media/holden/android/Pictures/Itunes\ Pictures/; read';;
 		esac
 		;;
     "Expansion") $TERMINAL -e zsh -c 'echo "Syncing Expansion disc in 2 seconds"; sleep 2; rsync -avP --delete /run/media/holden/HDD8T/Sync/ /run/media/holden/Expansion/Sync/; read';;
