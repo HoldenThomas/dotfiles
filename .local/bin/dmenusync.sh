af3ecbe77e93ab715c2e40e6e5dccb2ea79c09da
Holden Thomas <holden@holdenthomas.xyz>
2022-08-20 07:26:39 -0400

    update dmenusync

--- .local/bin/dmenusync.sh
+++ .local/bin/dmenusync.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-input="$(printf "Website\\nNAS\\nTablet\\nExpansion" | dmenu -i -p "Select option: ")" || exit 1
+input="$(printf "Website\\nNAS\\nTablet\\nPhone\\nExpansion" | dmenu -i -p "Select option: ")" || exit 1
 
 case $input in
 	"Website")
@@ -15,20 +15,20 @@ case $input in
 		case $syncTablet in
 			"Music") $TERMINAL -e zsh -c 'echo "Syncing music in 2 seconds"; sleep 2; \
 				rsync -vrPO --no-perms --inplace --size-only --delete --exclude=.thumbnails --exclude=iTunes --exclude="YouTube Music"/"10 Hours" --exclude="YouTube Music"/Videos /run/media/holden/HDD8T/Sync/Music/ /run/media/holden/android/Music/; read';;
-			"Videos") $TERMINAL -e zsh -c 'echo "Syncing music in 2 seconds"; sleep 2; \
-				rsync -vrPO --no-perms --inplace --size-only --delete /run/media/holden/HDD8T/Sync/Videos/AMV/ /run/media/holden/android/Videos/AMV/; read';;
-			"Photos") $TERMINAL -e zsh -c 'echo "Syncing music in 2 seconds"; sleep 2; \
-				rsync -vrPO --no-perms --inplace --size-only --delete --exclude="iPod Photo Cache" /run/media/holden/HDD8T/Sync/Pictures/Itunes\ Pictures/ /run/media/holden/android/Pictures/Itunes\ Pictures/; read';;
+			"Videos") $TERMINAL -e zsh -c 'echo "Syncing videos in 2 seconds"; sleep 2; \
+				rsync -vrPO --no-perms --inplace --size-only --delete /run/media/holden/HDD8T/Sync/Videos/MV/AMV/ /run/media/holden/android/Videos/AMV/; read';;
+			"Photos") $TERMINAL -e zsh -c 'echo "Syncing photos in 2 seconds"; sleep 2; \
+				rsync -vrPO --no-perms --inplace --size-only --delete --exclude=Family/"Johns Pictures" /run/media/holden/HDD8T/Sync/Pictures/ /run/media/holden/android/Pictures/; read';;
 		esac ;;
 	"Phone")
-		syncTablet="$(printf "Music\\nVideos\\nPhotos\\n" | dmenu -i -p "Choose Phone sync option: ")" || exit 1
-		case $syncTablet in
+		syncPhone="$(printf "Music\\nVideos\\nPhotos\\n" | dmenu -i -p "Choose Phone sync option: ")" || exit 1
+		case $syncPhone in
 			"Music") $TERMINAL -e zsh -c 'echo "Syncing music in 2 seconds"; sleep 2; \
-				rsync -vrPO --no-perms --inplace --size-only --delete --exclude=.thumbnails --exclude=iTunes --exclude="YouTube Music"/"10 Hours" --exclude="YouTube Music"/Videos /run/media/holden/HDD8T/Sync/Music/ /run/media/holden/android/Music/; read';;
-			"Videos") $TERMINAL -e zsh -c 'echo "Syncing music in 2 seconds"; sleep 2; \
-				rsync -vrPO --no-perms --inplace --size-only --delete /run/media/holden/HDD8T/Sync/Videos/AMV/ /run/media/holden/android/Videos/AMV/; read';;
-			"Photos") $TERMINAL -e zsh -c 'echo "Syncing music in 2 seconds"; sleep 2; \
-				rsync -vrPO --no-perms --inplace --size-only --delete --exclude="iPod Photo Cache" /run/media/holden/HDD8T/Sync/Pictures/Itunes\ Pictures/ /run/media/holden/android/Pictures/Itunes\ Pictures/; read';;
+				rsync -vrPO --no-perms --inplace --size-only --delete --exclude=.thumbnails --exclude=iTunes --exclude="YouTube Music"/"10 Hours" --exclude="YouTube Music"/Videos /run/media/holden/HDD8T/Sync/Music/ ~/android/Internal\ shared\ storage/Music/; read';;
+			"Videos") $TERMINAL -e zsh -c 'echo "Syncing videos in 2 seconds"; sleep 2; \
+				rsync -vrPO --no-perms --inplace --size-only --delete /run/media/holden/HDD8T/Sync/Videos/MV/AMV/ ~/android/Internal\ shared\ storage/Videos/AMV/; read';;
+			"Photos") $TERMINAL -e zsh -c 'echo "Syncing photos in 2 seconds"; sleep 2; \
+				rsync -vrPO --no-perms --inplace --size-only --delete --exclude=Family/"Johns Pictures" /run/media/holden/HDD8T/Sync/Pictures/ ~/android/Internal\ shared\ storage/Pictures/; read';;
 		esac ;;
     "Expansion") $TERMINAL -e zsh -c 'echo "Syncing Expansion disc in 2 seconds"; sleep 2; rsync -avP --delete /run/media/holden/HDD8T/Sync/ /run/media/holden/Expansion/Sync/; read';;
 esac
