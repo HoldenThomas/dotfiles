2eae788f79d8fbc471bb429c4e8b377c8ff9ed40
Holden Thomas <holden@holdenthomas.xyz>
2022-09-27 16:07:23 -0400

    update sb clock

--- .local/bin/statusBar/sb-clock.sh
+++ .local/bin/statusBar/sb-clock.sh
@@ -4,4 +4,4 @@ case $BLOCK_BUTTON in
 	1) notify-send "This Month" "$(cal --color=always | sed "s/..7m/<b><span color=\"red\">/;s/..27m/<\/span><\/b>/")";;
 esac
 
-date "+(%a)%d/%m/%y %R"
+date "+(%a)%D %I:%M%p"
