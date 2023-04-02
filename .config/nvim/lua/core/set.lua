e32c3affe4184b0dfdae62e47e9d2067c9e9d24b
holden <spiritupload@protonmail.com>
2023-04-02 17:06:45 -0400

    update nvim and tmux config

--- .config/nvim/lua/core/set.lua
+++ .config/nvim/lua/core/set.lua
@@ -8,6 +8,7 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 vim.opt.wrap = false
+
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
@@ -19,6 +20,7 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
 
 vim.opt.updatetime = 50
 
